771855058cfa97202b0e0ab7723125f6d407ae10
ievgen.kliuchyk <kliuchyk@gmail.com>
2023-01-09 12:21:17 +0100

    add files upload and refactor imports

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 
-import checkAuth from './utils/checkAuth.js';
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
+import { UserController, PostController } from './controllers/index.js';
 
 dotenv.config();
 const { DB_PASSWORD, DB_USER } = process.env;
@@ -22,16 +22,46 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.registerUser);
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/auth/login', [loginValidation, handleValidationErrors], UserController.login);
+app.post(
+  '/auth/register',
+  [registerValidation, handleValidationErrors],
+  UserController.registerUser,
+);
 app.get('/auth/me', checkAuth, UserController.getDetails);
 
 app.get('/posts', checkAuth, PostController.getAll);
 app.get('/posts/:id', checkAuth, PostController.getOne);
-app.post('/posts', [checkAuth, postCreateValidation], PostController.create);
+app.post(
+  '/posts',
+  [checkAuth, postCreateValidation, handleValidationErrors],
+  PostController.create,
+);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, PostController.update);
+app.patch(
+  '/posts/:id',
+  [checkAuth, postCreateValidation, handleValidationErrors],
+  PostController.update,
+);
+
+app.post('/upload', [checkAuth, upload.single('image')], (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}/`,
+  });
+});
 
 app.listen(4444, () => {
   console.log('server is running on port 4444');
